1c12cb2889a524ad328035b10093debd15f9c9f8
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-12-03 16:57:51 +0100

    changes

--- src/components/Canvas/ThreeCanvas.jsx
+++ src/components/Canvas/ThreeCanvas.jsx
@@ -1,18 +1,15 @@
 import { GlobalCanvas, SmoothScrollbar } from '@14islands/r3f-scroll-rig';
 
 import '@14islands/r3f-scroll-rig/css';
+import WebGLBackground from './WebGLBackground';
 
 export default function ThreeCanvas() {
   return (
     <>
-      <article>
-        <h1>Hello World</h1>
-        <h3>Bye World</h3>
-      </article>
-
       <SmoothScrollbar />
-
-      <GlobalCanvas />
+      <GlobalCanvas>
+        <WebGLBackground />
+      </GlobalCanvas>
     </>
   );
 }
--- src/components/Canvas/WebGLBackground.jsx
+++ src/components/Canvas/WebGLBackground.jsx
@@ -1,9 +1,26 @@
-import React, { useEffect, useSuspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 
-import { useFrame, useThree } from 'react-three-fiber';
+import { useThree, useFrame } from '@react-three/fiber';
 
 import { Image } from '@react-three/drei';
 
 export default function WebGLBackground() {
-  return <div></div>;
+  const bg = useRef();
+  const viewport = useThree((state) => state.viewport);
+
+  useFrame((_, delta) => {
+    if (bg.current) bg.current.rotation.z -= delta * 0.1;
+  });
+
+  return (
+    <Suspense fallback={null}>
+      <Image
+        ref={bg}
+        scale={Math.max(viewport.width, viewport.height) * 1.4}
+        url="images/background.jpg"
+        transparent
+        renderOrder={-1}
+      />
+    </Suspense>
+  );
 }
