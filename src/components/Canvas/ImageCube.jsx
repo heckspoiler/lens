fdb4d3f39b4ee4d216e47c40fb43fba465256b7a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-12-04 11:17:23 +0100

    changes

---
+++ src/components/Canvas/ImageCube.jsx
@@ -0,0 +1,19 @@
+import React, { useRef } from 'react';
+import {
+  ScrollScene,
+  UseCanvas,
+  useScrollbar,
+  useScrollRig,
+  styles,
+  useImageAsTexture,
+} from '@14islands/r3f-scroll-rig';
+
+import { useFrame } from '@react-three/fiber';
+
+import { MeshWobbleMaterial } from '@react-three/drei';
+
+import { a, useSpring, config } from 'react-spring/three';
+
+export default function ImageCube() {
+  return <div>ImageCube</div>;
+}
